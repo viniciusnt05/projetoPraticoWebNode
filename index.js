524997fbd7d925c6e55ca31e7c34691e14ff4193
viniciusnt05 <viniciusnt05@gmail.com>
2024-09-08 12:53:57 -0300

    projeto pratico web

--- index.js
+++ index.js
@@ -1,114 +1,157 @@
-const express = require("express"); 
-const app = express(); 
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
 
 const connection = require('./database/database');
 const perguntaModel = require("./database/pergunta");
 const respostaModel = require("./database/resposta");
 
-connection.authenticate().then(() =>{
-    console.log("Connected")
-})
+connection.authenticate()
+    .then(() => {
+        console.log("Connected");
+    })
+    .catch(err => {
+        console.log("Error connecting to the database:", err);
+    });
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// ----------------------------------------------------------------------------
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/indexPerguntas', (req, res) => {
+    perguntaModel.findAll({ raw: true, order: [['id', 'DESC']] })
+        .then(perguntas => {
+            res.render('listar', {
+                perguntas: perguntas
+            });
+        }).catch(err => {
+        res.send("Error: " + err);
+    });
+});
+
+app.get("/storePerguntas", (req, res) => {
+    res.render('pergunta');
+});
+
+app.post('/question/delete/:id', (req, res) => {
+    const perguntaId = req.params.id;
+
+    perguntaModel.destroy({
+        where: { id: perguntaId }
+    }).then(() => {
+        res.redirect('/');
+    }).catch(err => {
+        console.log("Erro ao apagar pergunta: " + err);
+        res.redirect('/');
+    });
+});
 
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
- 
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
-
-app.post("/savequestion", (request, response) =>{
-    const titule = request.body.titule
-    const description = request.body.description
+app.post("/salvarQuestao", (req, res) => {
+    const { titule, description } = req.body;
 
     perguntaModel.create({
         titulo: titule,
         descricao: description
     }).then(() => {
-        response.redirect('/')
-    })
-})
+        res.redirect('/');
+    }).catch(err => {
+        res.send("Error: " + err);
+    });
+});
 
-app.post('/responder', (request, response) =>{
-    var corpo = request.body.corpo
-    var perguntaId = request.body.pergunta
+app.get('/respostas/:id', (req, res) => {
+    const perguntaId = req.params.id;
+    perguntaModel.findByPk(perguntaId).then(pergunta => {
+        if (pergunta != undefined) {
+            respostaModel.findAll({
+                where: { perguntaId: perguntaId }
+            }).then(respostas => {
+                res.render('lerResposta', {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+app.post('/responder', (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
 
     respostaModel.create({
         corpo: corpo,
         perguntaId: perguntaId
     }).then(() => {
-        response.redirect('/question/' + perguntaId)
-    })
-})
+        res.redirect('/respostas/' + perguntaId);
+    }).catch((err) => {
+        console.log("Erro ao criar resposta: " + err);
+        res.redirect('/');
+    });
+});
 
-app.get('/', (requestion, response) => {
-    perguntaModel.findAll({raw: true, order:[['id', 'DESC']]}).then(pergunta => {
-        response.render('index', {
-            perguntas: pergunta
-        })
-    })
-})
 
-app.get('/question/:id', (requestion, response) => {
-    var id = requestion.params.id
+app.get('/responder/:id', (req, res) => {
+    const perguntaId = req.params.id;
 
-    perguntaModel.findOne({
-        where: {id :id}
-    }).then(pergunta => {
-        if(pergunta != undefined){
+    perguntaModel.findByPk(perguntaId).then(pergunta => {
+        if (pergunta != undefined) {
             respostaModel.findAll({
-                where: {perguntaId: pergunta.id}
+                where: { perguntaId: perguntaId }
             }).then(respostas => {
-                response.render('detalhepergunta', {
+                res.render('responder', {
                     pergunta: pergunta,
                     respostas: respostas
-                })
-            })
-        }else{
-            console.log("n tem")
+                });
+            });
+        } else {
+            res.redirect('/');
         }
-    })
-})
+    });
+});
 
-app.get("/question", (request, response) =>{
-    response.render('question')
-})
+app.post('/resposta/delete/:id', (req, res) => {
+    const respostaId = req.params.id;
 
-app.get('/resposta/delete/:id',(req,res)=>{
-    var id = req.params.id
     respostaModel.destroy({
-        where: {
-            id: id
-        }
-    }).then(()=>{
-        res.redirect('back')
-    })
-})
+        where: { id: respostaId }
+    }).then(() => {
+        res.redirect('back');
+    }).catch(err => {
+        console.log("Erro ao apagar resposta: " + err);
+        res.redirect('back');
+    });
+});
 
-// app.get("/:name/:language", function (req, res) { 
-//     const name = req.params.name
-//     const dev = req.params.language
-//     const showMensage = false
-//     const products = [
-//         {name: "Keyboard", price: 150},
-//         {name: "Monitor", price: 150}
-//     ]
+app.post('/respostas/deleteall/:id', (req, res) => {
+    const perguntaId = req.params.id;
 
-//    res.render('index', {
-//         name: name,
-//         dev: dev,
-//         company: "Unifae",
-//         salary: 10000,
-//         showMensage: showMensage,
-//         products: products
-//    }); 
-// }); 
- 
-app.listen(8181, function (erro) { 
-   if (erro) { 
-       console.log("Erro"); 
-   } else { 
-       console.log("Servidor iniciado..."); 
-   } 
-}); 
+    respostaModel.destroy({
+        where: { perguntaId: perguntaId }
+    }).then(() => {
+        res.redirect('back');
+    }).catch(err => {
+        console.log("Erro ao apagar todas as respostas: " + err);
+        res.redirect('back');
+    });
+});
+
+
+// Servidor
+app.listen(3000, (err) => {
+    if (err) {
+        console.log("Erro ao iniciar servidor");
+    } else {
+        console.log("Servidor iniciado na porta 8181...");
+    }
+});
